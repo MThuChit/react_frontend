eb6967b7f8e6ddb4aae37cb2d063c906d9f4a068
MThuChit <maythu111016@gmail.com>
2026-01-23 14:58:58 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import {useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import TestAPI from "./components/test_api";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState('...Loading...');
-
-  async function fetchData() {
-    const result = await fetch('http://localhost:3000/api/hello');
-    const data = await result.json();
-    console.log("result:", result);
-    console.log("data:", data);
-    setMessage(data.message);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      Message: {message}
-    </div>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<TestAPI />} />
+        </Routes>
+    );
 }
-
-export default App
+export default App;
